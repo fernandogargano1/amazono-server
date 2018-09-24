40246ccb76ce71dcf3c47200dfb8b5d843a71de6
Fernando Gargano <fernando@google.com>
2018-09-24 19:28:55 +0200

    Added Get Products and Get Products By Id Route Handlers

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,40 @@ const router = require('express').Router();
 const async = require('async');
 const Category = require('../models/category');
 const Product = require('../models/product');
+
+router.get('/products', async (req, res, next) => {
+    // This handler lacks some validation, ex: validate page number
+    const perPage = 10; 
+
+    const page = req.query.page || 1 ;
+    console.log('page: ', page);    
+
+    try {
+        const totalProducts = await Product.count({});
+
+        const products = await Product.find({})
+            .skip(perPage * (page - 1))
+            .limit(perPage)
+            .populate('category')
+            .populate('owner')
+            .exec();        
+
+        res.json({
+            success: true,
+            message: 'category',
+            products: products,            
+            totalProducts: totalProducts,
+            pages: Math.ceil(totalProducts / perPage)
+        });
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error['message']
+        });
+    }    
+});
+
 router.route('/categories')
     .get(async (req, res, next) => {
         try {
@@ -73,6 +107,29 @@ router.get('/categories/:id', async (req, res, next) => {
             success: false,
             message: error['message']
         });
+    }    
+});
+
+router.get('/product/:id', async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        .populate('category')
+        .populate('owner')
+        .exec();
+
+        if (!product) {
+            return res.json({
+                success: false,
+                message: 'Product is not found' 
+            });
+        }
+        res.json({
+            success: true,
+            product: product
+        });
+        
+    } catch (err) {
+        next(err);
     }    
 });
 
